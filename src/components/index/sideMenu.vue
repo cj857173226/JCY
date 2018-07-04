9bba8c4f35bbb771c20997701c5edad63513d9a9
xiaoliu <857173226@qq.com>
2018-07-04 15:00:26 +0800

    更改跳转路径

--- src/components/index/sideMenu.vue
+++ src/components/index/sideMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <el-menu id="menu" class="el-menu-vertical-demo"  :default-openeds="openeds">
-    <router-link to="/">
+    <router-link to="/home">
       <el-menu-item index="1" :class="isThisNav == '主页'?'is-active':''" @click="switchNav('主页')">
         <i class="el-icon-menu"></i>
         <span slot="title">主页</span>
@@ -14,28 +14,28 @@
       </template>
       <el-menu-item-group >
         <template slot="title" id="menu-group-title" style="display:none"></template>
-        <router-link to="/reportCue">
+        <router-link to="/home/reportCue">
           <el-menu-item index="2-1" :class="isThisNav == '举报线索'?'is-active':''" @click="switchNav('举报线索')" >
             <i class="el-icon-location"></i>
             <span slot="title">举报线索</span>
           </el-menu-item>
         </router-link>
 
-        <router-link to="/internetCue">
+        <router-link to="/home/internetCue">
           <el-menu-item index="2-2" :class="isThisNav == '互联网线索'?'is-active':''" @click="switchNav('互联网线索')">
             <i class="el-icon-location"></i>
             <span slot="title">互联网线索</span>
           </el-menu-item>
         </router-link>
 
-       <router-link to="/welfareCue">
+       <router-link to="/home/welfareCue">
          <el-menu-item index="2-3" :class="isThisNav == '公益组织线索'?'is-active':''" @click="switchNav('公益组织线索')">
            <i class="el-icon-location"></i>
            <span slot="title">公益组织线索</span>
          </el-menu-item>
        </router-link>
 
-        <router-link to="/heartCue">
+        <router-link to="/home/heartCue">
           <el-menu-item index="2-4" :class="isThisNav == '热点线索'?'is-active':''" @click="switchNav('热点线索')">
             <i class="el-icon-location"></i>
             <span slot="title">热点线索</span>
@@ -44,7 +44,7 @@
 
       </el-menu-item-group>
     </el-submenu>
-    <router-link to="/followCue" >
+    <router-link to="/home/followCue" >
       <el-menu-item index="3" :class="isThisNav == '关注线索'?'is-active':''" @click="switchNav('关注线索')">
         <i class="el-icon-menu"></i>
         <span slot="title">关注线索</span>
@@ -52,14 +52,14 @@
     </router-link>
 
 
-    <router-link to="/news">
+    <router-link to="/home/news">
       <el-menu-item index="4" :class="isThisNav == '新闻页'?'is-active':''" @click="switchNav('新闻页')">
         <i class="el-icon-document"></i>
         <span slot="title">新闻动态</span>
       </el-menu-item>
     </router-link>
 
-    <router-link to="/knowledge">
+    <router-link to="/home/knowledge">
       <el-menu-item index="5" :class="isThisNav == '知识库'?'is-active':''" @click="switchNav('知识库')">
         <i class="el-icon-setting"></i>
         <span slot="title">知识库</span>
@@ -67,7 +67,7 @@
     </router-link>
 
 
-    <router-link to="/dataAnalysis">
+    <router-link to="/home/dataAnalysis">
       <el-menu-item index="6" :class="isThisNav == '数据分析'?'is-active':''" @click="switchNav('数据分析')">
         <i class="el-icon-setting"></i>
         <span slot="title">数据分析</span>
