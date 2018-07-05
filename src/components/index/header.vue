a4eeb940c5f0b6cf779ee7581a333e05a3e50950
xiaoliu <857173226@qq.com>
2018-07-05 17:12:13 +0800

    互联网线索 接口完毕

--- src/components/index/header.vue
+++ src/components/index/header.vue
@@ -4,9 +4,9 @@
         <b>公益诉讼线索采集系统</b>
       </a>
       <nav class="navbar clearfix" >
-        <a href="#" class="menu-toggle">
-          <i class="el-icon-sort"></i>
-        </a>
+        <!--<a href="#" class="menu-toggle">-->
+          <!--<i class="el-icon-sort"></i>-->
+        <!--</a>-->
 
         <div class="navbar-right">
             <ul class="navbar-menu clearfix">
