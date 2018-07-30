59ab0f7e2b2e2bc1933171a32160fbd0206cbcf9
Wangxin <wangxin@name.com>
2018-07-30 09:20:13 +0800

    提交

--- src/components/pages/dataAnalysis/search.vue
+++ src/components/pages/dataAnalysis/search.vue
@@ -123,11 +123,12 @@ export default {
     }
     #pie-chart{
       width: 100%;
-      height: 100px;
+      height: 50px;
       margin:0 auto;
 
     }
     #word-cloud {
+      /*display: none;*/
       width: 80%;
       height: 350px;
       margin:0 auto;
